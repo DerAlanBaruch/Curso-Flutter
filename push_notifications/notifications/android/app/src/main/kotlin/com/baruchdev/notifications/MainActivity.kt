0b7d12c155af1fc0b770ca6d572db64f00be02af
baruch <tkma1997@gmail.com>
2022-04-29 18:57:21 -0500

    Push notifications

---
+++ push_notifications/notifications/android/app/src/main/kotlin/com/baruchdev/notifications/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.baruchdev.notifications
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ push_notifications/notifications/lib/services/notifications_service.dart
@@ -0,0 +1,6 @@
+// SHA1 D7:12:A8:FF:32:F5:53:31:9A:06:C4:95:9B:95:90:67:AF:54:44:2F
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationsService {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+}
