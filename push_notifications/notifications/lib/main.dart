7b0411a08623a9d0c75e8f026133d7126823be71
baruch <tkma1997@gmail.com>
2022-04-29 20:01:04 -0500

    Notifications push

--- push_notifications/notifications/lib/main.dart
+++ push_notifications/notifications/lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:notifications/screens/screens.dart';
 
-void main() => runApp(const MyApp());
+import 'services/notifications_service.dart';
 
-class MyApp extends StatelessWidget {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  NotificationsService.initilizeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> keyNavigatorState =
+      GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> keyScaffoldState =
+      GlobalKey<ScaffoldMessengerState>();
+  @override
+  void initState() {
+    super.initState();
+    NotificationsService.messageStream.listen((message) {
+      print('MyApp: $message');
+      final snakBar = SnackBar(
+        content: Text(message),
+      );
+      keyScaffoldState.currentState?.showSnackBar(snakBar);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +39,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Material App',
         initialRoute: 'home',
+        navigatorKey: keyNavigatorState,
+        scaffoldMessengerKey: keyScaffoldState,
         routes: {
           'home': (_) => const HomeScreen(),
           'message': (_) => const MessageScreen()
--- push_notifications/notifications/lib/services/notifications_service.dart
+++ push_notifications/notifications/lib/services/notifications_service.dart
@@ -1,6 +1,41 @@
 // SHA1 D7:12:A8:FF:32:F5:53:31:9A:06:C4:95:9B:95:90:67:AF:54:44:2F
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class NotificationsService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+  static final StreamController<String> _messageStream =
+      StreamController.broadcast();
+  static Stream<String> get messageStream => _messageStream.stream;
+
+  static Future _backgroundHandler(RemoteMessage message) async {
+    print(message.data);
+    _messageStream.add(message.data['type'] ?? 'No title');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print(message.data);
+    _messageStream.add(message.data['type'] ?? 'No title');
+  }
+
+  static Future _onMessageOpenApp(RemoteMessage message) async {
+    print(message.data);
+    _messageStream.add(message.data['type'] ?? 'No title');
+  }
+
+  static Future initilizeApp() async {
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+    print(token);
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
+  }
+
+  static closeStreams() {
+    _messageStream.close();
+  }
 }
